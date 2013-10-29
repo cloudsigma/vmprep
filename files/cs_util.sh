1a9efc4172d5f3fd0dfee204bc601bd83dda2979
Viktor Petersson <vpetersson@wireload.net>
2013-10-29 16:04:17 -0700

    Add ability to turn a non-desktop version into a desktop version.

--- files/cs_util.sh
+++ files/cs_util.sh
@@ -2,7 +2,12 @@
 
 PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
 
-### System functions
+# Get system information
+
+## Returns 'Debian', 'Ubuntu', 'Fedora', CentOS  etc.
+DIST=$(python -c 'import platform; dist = platform.linux_distribution()[0]; print dist[0].upper() + dist[1:]')
+
+# System functions
 function get_home {
     echo $(awk -F: -v v=$1 '{if ($1==v) print $6}' /etc/passwd)
 }
@@ -16,8 +21,8 @@ function create_ssh_folder {
     chown -R $1:$1 $BASE/.ssh
 }
 
-## Install the SSH key if present.
-## $1 is is the user account.
+# Install the SSH key if present.
+# $1 is is the user account.
 function set_ssh_key {
   # Set the target to the variable passed, but fall back to the current user.
   if [ -z "$1" ]; then
@@ -50,6 +55,27 @@ function install_newrelic {
 }
 
 
+function install_desktop_ubuntu {
+  sudo apt-get install ubuntu-desktop
+}
+
+function install_desktop_centos {
+  sudo yum -y groupinstall "Desktop" "Desktop Platform" "X Window System" "Fonts"
+  sudo sed -e 's/id:3:initdefault:/id:5:initdefault:/g' -i /etc/inittab
+}
+
+function install_desktop {
+  if  [ $DIST = 'Ubuntu' ]; then
+    install_desktop_ubuntu
+  elif [ $DIST = 'CentOS' ]; then
+    install_desktop_centos
+  elif [ $DIST = 'RedHat' ]; then
+    install_desktop_centos
+  else
+    echo "$DIST is an unsupported Linux distribution"
+  fi
+}
+
 #### Process user input
 case "$1" in
   set-ssh-key)
@@ -60,6 +86,9 @@ case "$1" in
     ;;
   install-newrelic)
     install_newrelic
+    ;;
+  install-desktop)
+    install_desktop
     ;;
   *)
     echo -e 'Valid options are:'
